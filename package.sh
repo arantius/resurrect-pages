15d99e4f52263f4d30125b2d4ea9be29da5a8000
Anthony Lieuallen <arantius@gmail.com>
2007-02-19 15:15:19 +0000

    one global package script

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PROJ=`sed '2,$d' chrome.manifest|awk '{print $2}'`
-rm -f ${PROJ}*.xpi
-VER=`grep 'em:version' install.rdf | sed 's/[^0-9.]//g'`
-
-echo CREATING: ${PROJ}-${VER}.xpi
-
-zip -9 ${PROJ}-${VER}.xpi \
-	`find . -type d -name .svn -prune -false -o -type f -a -not -name package.sh`
